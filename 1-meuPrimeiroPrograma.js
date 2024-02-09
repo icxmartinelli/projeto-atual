741f96ce0175ca08bfcd0a4cfce6d50ab5aeabec
Isabella Martinelli <69216574+icxmartinelli@users.noreply.github.com>
2024-02-08 22:25:29 -0300

    Initial commit

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,8 @@
+/*
+.:: ANOTAÇÕES ::.
+08-02-24
+a ideia é usar o cmd ou powershell para ver o JS funcionando, não o navegador;
+comando 'node' no powershell ou no terminal aqui no vs code para executar o node;
+*/
+
+console.log("Meu primeiro programa");
