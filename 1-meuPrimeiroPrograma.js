531e69e41546666682f9ed52686ebe92cb6ab7d0
Isabella Martinelli <69216574+icxmartinelli@users.noreply.github.com>
2024-02-16 22:32:36 -0300

    lógica de programação

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -5,4 +5,13 @@ a ideia é usar o cmd ou powershell para ver o JS funcionando, não o navegador;
 comando 'node' no powershell ou no terminal aqui no vs code para executar o node;
 */
 
-console.log("Meu primeiro programa");
+console.log("Meu primeiro programa: trabalhando com variáveis");
+
+// 16-02-24 passando pela lógica de programação '-'
+// Running Average Power Level (RAPL) -> nome do terminal;
+const idade = 28;
+console.log("idade: " + idade);
+
+// Cammel Case -> segunda palavra com letra maiúscula
+const idadeSomada = idade + 10;
+console.log("idadeSomada: " + idadeSomada);
---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,8 @@
+// 16-02-24 -> console é com 'c' minúsculo; JS é Case Sensitive;
+console.log("Trabalhando com Variáveis");
+
+// ou todas as variáveis com minúscula ou com minúscula; idade é diferente de Idade;
+// JS permite criar variável sem const '-' mas é uma boa prática declarar;
+// JS não é fortemente tipado
+// não tem regra nenhuma nisso, Deus...
+// é possível declarar com 'let' ou 'const';
